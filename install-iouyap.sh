3ab387337ae1ca78cb73b2214a8c288deca4c472
profmancusoa <profmancusoa@gmail.com>
2025-04-30 09:05:44 +0200

    fix compilation of iniparser

--- install-iouyap.sh
+++ install-iouyap.sh
@@ -16,11 +16,15 @@ dnf -y install git bison flex
 cd /tmp
 git clone http://github.com/ndevilla/iniparser.git
 cd iniparser
+mkdir build
+cd build
+cmake ..
 make
 sudo cp libiniparser.* /usr/lib/
 sudo cp src/iniparser.h /usr/local/include
 sudo cp src/dictionary.h /usr/local/include
 cd ..
+rm -f /tmp/iniparser
 
 git clone https://github.com/GNS3/iouyap.git
 cd iouyap
