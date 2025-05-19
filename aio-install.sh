8adf4fda21361d9c28e9229d262f8fcaccb933b4
profmancusoa <profmancusoa@gmail.com>
2025-05-19 12:58:52 +0200

    add image links

--- aio-install.sh
+++ aio-install.sh
@@ -5,3 +5,4 @@ sudo ./install-gns3-server.sh
 sudo ./install-gns3-gui.sh
 sudo ./install-gns3-gui-shortcut.sh
 sudo ./install-vpcs.sh
+./link_images.sh
---
+++ link_images.sh
@@ -0,0 +1,13 @@
+#Antonio Mancuso <lprofmancusoa@gmail.com>
+#!/bin/bash
+
+
+ln -s /opt/GNS3/images/IOS/c2691-adventerprisek9-mz.124-15.T14.image ~/GNS3/images/IOS/c2691-adventerprisek9-mz.124-15.T14.image
+
+ln -s /opt/GNS3/images/IOS/c3745-adventerprisek9-mz.124-25d.image ~/GNS3/images/IOS/c3745-adventerprisek9-mz.124-25d.image
+
+ln -s /opt/GNS3/images/IOU/i86bi_LinuxL2-AdvEnterpriseK9-M_152_May_2018.bin ~/GNS3/images/IOU/i86bi_LinuxL2-AdvEnterpriseK9-M_152_May_2018.bin
+
+ln -s /opt/GNS3/images/IOU/i86bi_LinuxL3-AdvEnterpriseK9-M2_157_3_May_2018.bin ~/GNS3/images/IOU/i86bi_LinuxL3-AdvEnterpriseK9-M2_157_3_May_2018.bin
+
+ln -s /opt/GNS3/images/QEMU/alpine-GNS3.qcow2 ~/GNS3/images/QEMU/alpine-GNS3.qcow2
